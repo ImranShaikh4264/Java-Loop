11b899ccff00e1a6d1a794fffaded0304fda74ed
ImranShaikh4264 <shaikhimrana56@gmail.com>
2022-10-05 16:08:09 +0530

    Add: Java program to find sum of natural number using For loop

---
+++ SumOfNaturalFor.java
@@ -0,0 +1,12 @@
+package Day2;
+
+public class SumOfNaturalFor {
+	public static void main(String[] args) {
+		int number = 10, sum = 0;
+		for ( int i = 1; i <= number; ++i )
+			sum += i;
+	
+
+    System.out.println("sum = " + sum);
+}
+}
