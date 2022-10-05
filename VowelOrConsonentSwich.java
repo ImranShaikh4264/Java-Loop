3916adb53ac37b0f6761c396dd4ebee8b796062b
ImranShaikh4264 <shaikhimrana56@gmail.com>
2022-10-05 16:13:26 +0530

    Add: Java program to check given character is Vowel or consonent using switch case statement

---
+++ VowelOrConsonentSwich.java
@@ -0,0 +1,27 @@
+package Day2;
+
+public class VowelOrConsonentSwich {
+	public static void main(String[] args) {
+		
+	 char Alphabate = 'i';
+     switch (Alphabate) {
+         case 'a':
+             System.out.println("Alphabata is Vowel");
+             break;
+         case 'e':
+             System.out.println("Alphabata is Vowel");
+             break;
+         case 'i':
+             System.out.println("Alphabata is Vowel");
+             break;
+         case 'o':
+             System.out.println("Alphabata is Vowel");
+             break;
+         case 'u':
+             System.out.println("Alphabata is Vowel");
+             break;
+         default:
+             System.out.println("Alphabate is consonents");    
+     }
+	}
+}
